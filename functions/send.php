95da652bbeb0aeac2fd302629492a4aefc29330f
Nikita Deich <54121126+De1ch@users.noreply.github.com>
2019-12-12 13:15:07 +0600

    sending messages (dev.1)

--- functions/send.php
+++ functions/send.php
@@ -1,14 +1,8 @@
 <?php
-/*
-Форма обратной связи может получать сообщения с любых почтовых ящиков.
-Исправлена проблема кодировки при получении писем почтовым клиентом Outlook.
-Вы скачали её с сайта Epic Blog https://epicblog.net Заходите на сайт снова!
-ВНИМАНИЕ! Лучше всего в переменную myemail прописать почту домена, который использует сайт. А не mail.ru, gmail и тд.
-*/
 if(isset($_POST['submit'])){
-/* Устанавливаем e-mail Кому и от Кого будут приходить письма */
-$to = "work.deich@gmail.com"; // Здесь нужно написать e-mail, куда будут приходить письма
-$from = "ricardo.channel17@gmail.com"; // Здесь нужно написать e-mail, от кого будут приходить письма, например no-reply(собака)epicblog.net
+
+$to = "work.deich@gmail.com"; /* кому отправляюттся сообщения */
+$from = "no-reply@victim.kz";  /* от кого отправляются сообщения */
 
 /* Указываем переменные, в которые будет записываться информация с формы */
 $first_name = $_POST['first_name'];
@@ -17,12 +11,30 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-/* Проверка правильного написания e-mail адреса */
-if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
-{
-$show_error[] = ("<br /> Е-mail адрес не существует");
+if (strlen($data['first_name']) < 3) { //проверка на количество символов
+    $errors[] = 'Укажите имя!';
 }
 
+/* Проверка правильного написания e-mail адреса */
+if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email)) {
+  $error[] = ("Е-mail адрес введен неверно!");
+}
+
+if (strlen($data['message']) > 20) { //проверка на количество символов
+  $errors[] = 'Введите неменее 20 слов в сообщение!';
+}
+
+if (empty($error)) {
+  /* Отправка сообщения, с помощью функции mail() */
+  mail($to, $subject, $mail_to_myemail, $headers . 'Content-type: text/plain; charset=utf-8');
+  echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
+  echo "<br /><br /><a href='http://victim.kz/'>Вернуться на сайт.</a>";
+} else {
+  echo '<div style="color: #F95959;">'.array_shift($error).'</div>';
+  echo "<br /><a href='http://victim.kz/#fh5co-consult'>Вернуться к отправке сообщения.</a>";
+}
+
+$headers = "From: $from \r\n";
 /* Переменная, которая будет отправлена на почту со значениями, вводимых в поля */
 $mail_to_myemail = "Здравствуйте!
 Было отправлено сообщение с сайта!
@@ -31,17 +43,8 @@ $mail_to_myemail = "Здравствуйте!
 E-mail: $email
 Текст сообщения: $message
 Чтобы ответить на письмо, создайте новое сообщение, скопируйте электронный адрес и вставьте в поле Кому.";
-
-$headers = "From: $from \r\n";
-
-/* Отправка сообщения, с помощью функции mail() */
-mail($to, $subject, $mail_to_myemail, $headers . 'Content-type: text/plain; charset=utf-8');
-echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
-echo "<br /><br /><a href='http://victim.kz/'>Вернуться на сайт.</a>";
 }
 ?>
 <!--Переадресация на главную страницу сайта, через 3 секунды-->
-<script language="JavaScript" type="text/javascript">
-function changeurl(){eval(self.location="http://victim.kz/");}
-window.setTimeout("changeurl();",3000);
+
 </script>
